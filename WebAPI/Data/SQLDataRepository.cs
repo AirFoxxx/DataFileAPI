240139f8de64ab53fc1c4bb23589a65968799c83
Koci <kociandreas@seznam.cz>
2021-11-16 21:02:42 +0100

    Added Controller, added service injection config, added basic GET

---
+++ WebAPI/Data/SQLDataRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class SQLDataRepository : IDataRepository
+    {
+        public Task<bool> AddFileAsync(DataFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteFileAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataFile>> GetAllFilesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataFile> GetFileByNameAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Data;
 
 namespace WebAPI
 {
@@ -26,12 +27,12 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
             });
+            services.AddScoped<IDataRepository, SQLDataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
