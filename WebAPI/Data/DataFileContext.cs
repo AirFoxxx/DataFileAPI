848c88879cb5decb7db355c14a91f322ef60a56f
Koci <kociandreas@seznam.cz>
2021-11-16 21:19:31 +0100

    Created DBContext representation for DataFiles

---
+++ WebAPI/Data/DataFileContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class DataFileContext : DbContext
+    {
+        public DataFileContext(DbContextOptions<DataFileContext> options) : base(options)
+        {
+        }
+
+        // Model representation for DataFiles
+        public DbSet<DataFile> DataFiles { get; set; }
+    }
+}
