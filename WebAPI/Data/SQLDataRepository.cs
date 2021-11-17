c0b34221b390a72e4844c63952755ef049e559ab
Koci <kociandreas@seznam.cz>
2021-11-17 20:06:02 +0100

    Made extensions work even without periods specified

--- WebAPI/Data/SQLDataRepository.cs
+++ WebAPI/Data/SQLDataRepository.cs
@@ -6,6 +6,7 @@ using WebAPI.Models;
 
 namespace WebAPI.Data
 {
+    /// <inheritdoc />
     public class SQLDataRepository : IDataRepository
     {
         private readonly DataFileContext _context;
@@ -15,32 +16,41 @@ namespace WebAPI.Data
             _context = context;
         }
 
+        /// <inheritdoc />
         public async Task<DataFile> AddFileAsync(DataFile file)
         {
             var obj = await _context.DataFiles.AddAsync(file);
             return obj.Entity;
         }
 
+        /// <inheritdoc />
         public async Task<bool> DeleteFileAsync(int id)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public IEnumerable<DataFile> GetAllFiles()
         {
             return _context.DataFiles.ToList();
         }
 
+        /// <inheritdoc />
         public DataFile GetFileById(int id)
         {
             return _context.DataFiles.FirstOrDefault(obj => obj.Id == id);
         }
 
+        /// <inheritdoc />
         public IEnumerable<DataFile> GetFilesByExtension(string extension)
         {
-            return _context.DataFiles.Where(obj => obj.Extension.ToLower() == extension.ToLower()).ToList();
+            return _context.DataFiles
+                .Where(obj => obj.Extension.ToLower()
+                .Replace(".", string.Empty) == extension.ToLower()
+                .Replace(".", string.Empty)).ToList();
         }
 
+        /// <inheritdoc />
         public async Task SaveChangesAsync()
         {
             await _context.SaveChangesAsync();
